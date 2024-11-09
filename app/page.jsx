fd0cc7ae012578d9748fdb9da1ea459e7aeb254b
berkcankucukoglu <berkcankucukoglu@gmail.com>
2024-11-09 18:17:54 +0300

    initial commit

---
+++ app/page.jsx
@@ -0,0 +1,8 @@
+export default function Home() {
+  return (
+    <main>
+      <p>Lorem ipsum dolor sit amet.
+      </p>
+    </main>
+  );
+}
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,17 +1,35 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./app/**/*.{js,ts,jsx,tsx,mdx}",
-  ],
-  theme: {
-    extend: {
-      colors: {
-        background: "var(--background)",
-        foreground: "var(--foreground)",
-      },
-    },
-  },
-  plugins: [],
+	darkMode: ["class"],
+	content: [
+		"./pages/**/*.{js,ts,jsx,tsx,mdx}",
+		"./components/**/*.{js,ts,jsx,tsx,mdx}",
+		"./app/**/*.{js,ts,jsx,tsx,mdx}",
+	],
+	theme: {
+		container: {
+			center: true,
+			padding: "16px",
+		},
+		screens: {
+			sm: "640px",
+			md: "768px",
+			lg: "960px",
+			xl: "1024px",
+		},
+		fontFamily: {
+			primary: "var(--font-roboto)"
+		},
+		extend: {
+			colors: {
+				primary: '#020617',
+				secondary: '#27374D',
+				accent: {
+					DEFAULT: '#FFD369',
+					hover: '#F4CE14',
+				}
+			}
+		}
+	},
+	plugins: [require("tailwindcss-animate")],
 };
